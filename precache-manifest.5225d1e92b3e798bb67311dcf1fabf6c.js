3a6c61dd338f4a600bac87a499ac5dbf90514f90
Yliya Ring <ring.yuliya@gmail.com>
2020-07-13 15:14:38 +0300

    Updates

---
+++ precache-manifest.5225d1e92b3e798bb67311dcf1fabf6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede9270da04678acf581e91441a1c844",
+    "url": "/CatGenerator/index.html"
+  },
+  {
+    "revision": "d4338d621ff235704c99",
+    "url": "/CatGenerator/static/css/main.d7b2550e.chunk.css"
+  },
+  {
+    "revision": "8346a7c58c3fd768bcbd",
+    "url": "/CatGenerator/static/js/2.8c5ffa0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CatGenerator/static/js/2.8c5ffa0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4338d621ff235704c99",
+    "url": "/CatGenerator/static/js/main.dd5c6a33.chunk.js"
+  },
+  {
+    "revision": "2e1f87e6e9b22851471c",
+    "url": "/CatGenerator/static/js/runtime-main.7b90c173.js"
+  },
+  {
+    "revision": "d4376ad689aaf3478637a983042ce127",
+    "url": "/CatGenerator/static/media/defaultCat.d4376ad6.png"
+  }
+]);
